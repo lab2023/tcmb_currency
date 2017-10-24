af9d290c0fd97871fc5a30ab4c0b9b8daf8bf813
hamdibayhan <hamdi.bayhan@lab2023.com>
2017-10-24 16:26:31 +0300

    KBP-166 #time 2h - Rubocop was added and code was improved

--- lib/generators/tcmb_currency/initializer/initializer_generator.rb
+++ lib/generators/tcmb_currency/initializer/initializer_generator.rb
@@ -3,14 +3,12 @@ require 'rails/generators/active_record'
 
 module TcmbCurrency
   class InitializerGenerator < ::Rails::Generators::Base
+    source_root File.join(File.dirname(__FILE__), 'templates')
 
-      source_root File.join(File.dirname(__FILE__), 'templates')
+    desc 'Creates a sample Tcmb_Currency initializer.'
 
-      desc 'Creates a sample Tcmb_Currency initializer.'
-
-      def copy_initializer
-        copy_file 'money.rb', 'config/initializers/money.rb'
-      end
-
+    def copy_initializer
+      copy_file 'money.rb', 'config/initializers/money.rb'
     end
+  end
 end
--- lib/money/bank/tcmb_currency.rb
+++ lib/money/bank/tcmb_currency.rb
@@ -12,26 +12,26 @@ class Money
 
   module Bank
     class TcmbCurrency < Money::Bank::VariableExchange
-
       class CrossRate < ActiveRecord::Base
-
       end
 
       class CurrencyType < ActiveRecord::Base
-
       end
 
       attr_reader :rates
 
       def exchange_with!(*args)
-        from, to_currency, date = args[0], args[1], args[2]
+        from = args[0]
+        to_currency = args[1]
+        date = args[2]
         return from if same_currency?(from.currency, to_currency)
         rate = fetch_rate!(from.currency, to_currency, date)
         unless rate
-          raise UnknownRate, "No conversion rate known for '#{from.currency.iso_code}' -> '#{to_currency}'"
+          raise UnknownRate, "No conversion rate known for
+                '#{from.currency.iso_code}' -> '#{to_currency}'"
         end
-        _to_currency_ = Currency.wrap(to_currency)
-        fractional = BigDecimal.new(from.fractional.to_s) / (BigDecimal.new(from.currency.subunit_to_unit.to_s) / BigDecimal.new(_to_currency_.subunit_to_unit.to_s))
+        wrap_to_currency_ = Currency.wrap(to_currency)
+        fractional = get_fractional(from, wrap_to_currency_)
         ex = (fractional * BigDecimal.new(rate.to_s)).to_f
         ex = if block_given?
                yield ex
@@ -40,29 +40,37 @@ class Money
              else
                ex.to_s.to_i
              end
-        Money.new(ex, _to_currency_)
+        Money.new(ex, wrap_to_currency_)
       end
 
       private
 
+      def get_fractional(from, to_currency)
+        bd_from_fractional = BigDecimal.new(from.fractional.to_s)
+        bd_from_currency = BigDecimal.new(from.currency.subunit_to_unit.to_s)
+        bd_to_currency = BigDecimal.new(to_currency.subunit_to_unit.to_s)
+        bd_from_fractional / (bd_from_currency / bd_to_currency)
+      end
+
       def fetch_rate!(from, to, date)
-        from, to = Money::Currency.wrap(from), Money::Currency.wrap(to)
+        from_to_s = Money::Currency.wrap(from).to_s
+        to_to_s = Money::Currency.wrap(to).to_s
         from_rate = 1.0
         to_rate = 1.0
+        froms = Money::Bank::TcmbCurrency::CrossRate.where(code: from_to_s)
+        tos = Money::Bank::TcmbCurrency::CrossRate.where(code: to_to_s)
         if date.nil?
-          from_rate = Money::Bank::TcmbCurrency::CrossRate.where(code: from.to_s).last.rate.to_f unless from.to_s == 'USD'
-          to_rate = Money::Bank::TcmbCurrency::CrossRate.where(code: to.to_s).last.rate.to_f unless to.to_s == 'USD'
+          from_rate = froms.last.rate.to_f unless from_to_s == 'USD'
+          to_rate = tos.last.rate.to_f unless to_to_s == 'USD'
         else
-          if Money::Bank::TcmbCurrency::CrossRate.where(date: date).present?
-            from_rate = Money::Bank::TcmbCurrency::CrossRate.where(code: from.to_s, date: date).last.rate.to_f unless from.to_s == 'USD'
-            to_rate = Money::Bank::TcmbCurrency::CrossRate.where(code: to.to_s, date: date).last.rate.to_f unless to.to_s == 'USD'
-          else
+          unless Money::Bank::TcmbCurrency::CrossRate.where(date: date).present?
             raise 'There is no record in that date.'
           end
+          from_rate = froms.where(date: date).last.rate.to_f unless from_to_s == 'USD'
+          to_rate = tos.where(date: date).last.rate.to_f unless to_to_s == 'USD'
         end
-        to_rate/from_rate
+        to_rate / from_rate
       end
     end
   end
 end
-
